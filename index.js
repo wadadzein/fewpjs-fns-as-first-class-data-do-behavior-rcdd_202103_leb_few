1e51f5625470f249939fb0e7f8d34d37f5aa63fe
wadad zein <wadad_zein@hotmail.com>
2021-04-14 00:24:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+
+function greet(timeString) {
+  let time_number=parseInt(timeString,10);
+  if (time_number < 12){
+    return "Good Morning"
+  }
+  if (time_number >=12 && time_number <=17) {
+    return "Good Afternoon"
+  }
+  if (time_number > 17) {
+    return "Good Evening"
+  }
+  }
 /* Write your implementation of displayMessage() */
+//const displayMessage = greet(timeString);
+function displayMessage (m){
+ // greet(timeString)
+  let elemt = document.getElementById ("greeting").innerText=m;
+}
+
